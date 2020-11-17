0e15891f46efc9b6cccf843740a9fc9e79039edc
Nathan Menard <nathanmenard.work@gmail.com>
2020-11-17 17:55:40 +0100

    List all pets + seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Pet.create!(
+  species: "cat",
+  when: Date.today,
+  where: "La Cantine, Nantes",
+  photo_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.2xtbyxEs4kpIPIo33MotmQHaEo%26pid%3DApi&f=1"
+)
+
+Pet.create!(
+  species: "dog",
+  when: Date.today - 2.days,
+  where: "MacDo, Nantes",
+  photo_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.XDC_kGTkZWF0ySIQR9FrxAHaEK%26pid%3DApi&f=1"
+)
+
+Pet.create!(
+  species: "dragon",
+  when: "2020-11-20",
+  where: "Sky, Nantes",
+  photo_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.kjqYzJXjFanfN00z29nrXgHaEK%26pid%3DApi&f=1"
+)
